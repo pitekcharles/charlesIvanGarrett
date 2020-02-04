a514129085618c4723056600c2d8ef93fdb23435
iampopov <ivantsar@gmail.com>
2020-02-04 14:31:44 -0700

    resolved Unrecognized datatype for attribute Customer.name

--- config/config.js
+++ config/config.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
+    "username": process.env.username,
+    "password": process.env.db_pass,
+    "database": process.env.database,
     "host": "127.0.0.1",
     "dialect": "mysql",
     "operatorsAliases": false
