97c6f1b02cda8a5d53d17ea163ad5a7b5d4b7dd4
iampopov <ivantsar@gmail.com>
2020-02-04 14:06:04 -0700

    products model and products routes

---
+++ models/product.js
@@ -0,0 +1,29 @@
+module.exports = function (sequelize, DataTypes) {
+    const Product = sequelize.define("Product", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        quantity: {
+            type: DataTypes.INTEGER
+        },
+        cost: {
+            type: DataTypes.DECIMAL(7,2),
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.TEXT
+        },
+        category: {
+            type: DataTypes.STRING
+        }
+    }, {
+        freezeTableName: true 
+    })
+    
+    Product.associate = function(models) {
+        Product.belongsToMany(models.Order, { through: "productOrder" })
+    }
+
+    return Product;
+}
