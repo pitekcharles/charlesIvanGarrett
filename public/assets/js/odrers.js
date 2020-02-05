ded15c974ebb4683789cdcc8c648761b24bdf001
iampopov <ivantsar@gmail.com>
2020-02-05 14:36:11 -0700

    working on order.js front end js

--- public/assets/js/odrers.js
+++ public/assets/js/odrers.js
@@ -2,25 +2,56 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 /* eslint-disable func-names */
-/* eslint-plugin-disable angular */
 
 $(document).ready(function() {
   const orderContainer = $("#orderContainer");
   const orderCategorySelected = $("#category");
 
   // click events
-  $(document).on("click", "button.delete", handleOrderDelete); //!
-  $(document).on("click", "button.edit", handleOrderEdit); //!
+  $(document).on("click", "button.delete", handleOrderDelete);
+  $(document).on("click", "button.edit", handleOrderEdit);
   // Variable to hold our posts
   let orders;
 
-  function deleteOrder(params) {}
+  function getOrders(customer) {
+    customerId = customer || "";
+    if (customerId) {
+      customerId = `/?customer_id=${customerId}`;
+    }
+    $.get(`/api/posts${customerId}`, function(data) {
+      console.log("Orders ", data);
+      orders = data;
+      if (!orders || !orders.length) {
+        displayEmpty(customer);
+      } else {
+        initializeRows();
+      }
+    });
+  }
+
+  function initializeRows() {
+    blogContainer.empty();
+    const ordersToAdd = [];
+    for (const i = 0; i < orders.length; i++) {
+      ordersToAdd.push(createNewRow(posts[i]));
+    }
+    blogContainer.append(ordersToAdd);
+  }
+
+  function deleteOrder(id) {
+    $.ajax({
+      method: "DELETE",
+      url: `/api/posts/${id}`,
+    }).then(function() {
+      getOrders(orderCategorySelected.val());
+    });
+  }
 
   function handleOrderDelete() {
     const currentOrder = $(this)
       .parent()
       .data("order");
-    deleteOrder(currentOrder.id); //!
+    deleteOrder(currentOrder.id);
   }
 
   function handleOrderEdit() {
@@ -28,5 +59,20 @@ $(document).ready(function() {
       .parent()
       .data("post");
     window.location.href = `/cms?order_id=${currentOrder.id}`;
+  }
+
+  function displayEmpty(id) {
+    const query = window.location.search;
+    let partial = "";
+    if (id) {
+      partial = ` for Customer #${id}`;
+    }
+    blogContainer.empty();
+    const messageH2 = $("<h2>");
+    messageH2.css({ "text-align": "center", "margin-top": "50px" });
+    messageH2.html(
+      `No posts yet${partial}, navigate <a href='/cms${query}'>here</a> in order to get started.`
+    );
+    blogContainer.append(messageH2);
   }
 });
