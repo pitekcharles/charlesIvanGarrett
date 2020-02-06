0f00136632325e24f01bd4bc070e1e3ab6c09140
garrett krage <garrett.krage@gmail.com>
2020-02-06 14:17:42 -0700

    merged again

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,21 +6,6 @@ router.get("/", function(req, res) {
   res.render("index");
 });
 
-<<<<<<< HEAD
-// router.get("/customer", function (req, res) {
-//     // res.sendFile(path.join(__dirname, "../public/customer.html"));
-//     res.render("customer");
-=======
-// router.get("/customer", function(req, res) {
-//   // res.sendFile(path.join(__dirname, "../public/customer.html"));
-//   res.render("customer");
->>>>>>> e5b94be99b5344c29ed3808b3a81399e43107351
-// });
-
-// router.get("/product", function(req, res) {
-//   // res.sendFile(path.join(__dirname, "../public/product.html"));
-//   res.render("product");
-// });
 
 router.get("/order", function(req, res) {
   // res.sendFile(path.join(__dirname, "../public/order.html"));
