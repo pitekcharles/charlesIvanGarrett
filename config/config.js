ff19ab8b7c6ffff13ae6270551fa04deb531558e
iampopov <ivantsar@gmail.com>
2020-02-10 09:58:31 -0700

    updated config.js for heroku deployment

--- config/config.js
+++ config/config.js
@@ -22,7 +22,7 @@ module.exports = {
     password: null,
     database: "database_production",
     host: "127.0.0.1",
-    use_env_variable: "JAWSDB_URL"
+    use_env_variable: "JAWSDB_URL",
     dialect: "mysql",
     operatorsAliases: false,
   },
